35ab00ba3afa6236bc6b8742e2b7733a1bdeadfc
AbdulAhad2659 <102375793+AbdulAhad2659@users.noreply.github.com>
2023-05-31 12:47:37 +0500

    added filedialog to select a video file

--- Converter.py
+++ Converter.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+from tkinter import Tk, filedialog
 
 def convert_to_mp3(input_file, output_file):
     ffmpeg_cmd = [
@@ -12,4 +13,19 @@ def convert_to_mp3(input_file, output_file):
         "-y", output_file
     ]
 
-    
+    try:
+        subprocess.run(ffmpeg_cmd, check=True)
+        print("Video has been successfully converted.")
+
+    except subprocess.CalledProcessError as e:
+        print("Failed to convert.")
+
+
+root = Tk()
+root.withdraw()
+
+input_file = filedialog.askopenfilename(
+    title="Select a video file",
+    filetypes=[("Video files", "*.mp4;*.avi;*.mov"), ("All files", "*.*")]
+)
+
