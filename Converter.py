0e574c42383c4d4cfe86b0551acb742ff3781e4b
AbdulAhad2659 <102375793+AbdulAhad2659@users.noreply.github.com>
2023-05-31 12:40:14 +0500

    ffmpeg command for conversion

--- Converter.py
+++ Converter.py
@@ -1,3 +1,15 @@
 import os
 import subprocess
 
+def convert_to_mp3(input_file, output_file):
+    ffmpeg_cmd = [
+        "ffmpeg",
+        "-i", input_file,
+        "-vn",
+        "-acodec", "libmp3lame",
+        "-ab", "192k",
+        "-ar", "44100",
+        "-y", output_file
+    ]
+
+    
