8090ac372f0d4500dcbcf953a76926ad5f64dd3c
AbdulAhad2659 <102375793+AbdulAhad2659@users.noreply.github.com>
2023-05-31 13:07:16 +0500

    Added alerts for video conversion

--- Converter.py
+++ Converter.py
@@ -1,6 +1,6 @@
 import os
 import subprocess
-from tkinter import Tk, filedialog
+from tkinter import Tk, filedialog, messagebox
 
 def convert_to_mp3(input_file, output_file):
     ffmpeg_cmd = [
@@ -14,12 +14,12 @@ def convert_to_mp3(input_file, output_file):
     ]
 
     try:
-        save_path = filedialog.asksaveasfile(output_file)
+        save_path = filedialog.askdirectory()
         subprocess.run(ffmpeg_cmd, check=True)
-        print("Video has been successfully converted.")
+        messagebox.showinfo("Success", "Video has been successfully converted.")
 
     except subprocess.CalledProcessError as e:
-        print("Failed to convert.")
+        messagebox.showinfo("Error", "Failed to convert.")
 
 
 root = Tk()
@@ -27,6 +27,8 @@ root.withdraw()
 
 input_file = filedialog.askopenfilename(
     title="Select a video file",
-    filetypes=[("Video files", "*.mp4;*.avi;*.mov")]
+    filetypes=[("Video files", "*.mp4"), ("All files", "*.*")]
 )
+
+convert_to_mp3(input_file, "output.mp3")
 
